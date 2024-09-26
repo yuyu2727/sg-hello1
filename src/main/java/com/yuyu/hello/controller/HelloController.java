589dd056367125abcf50c52500692b8b0959b956
yu <yu@naver.com>
2024-09-26 17:15:09 +0900

    commit

--- src/main/java/com/yuyu/hello/controller/HelloController.java
+++ src/main/java/com/yuyu/hello/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 	@GetMapping("/hello")
 	public String helloPage(Model model) {
-		model.addAttribute("greeting", "Hello! SG");
+		model.addAttribute("greeting", "Hello! 안녕");
 		return "hello";
 		
 	}
